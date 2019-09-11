3fcd9c32ef89fd7857d57928dd2656fe35e99a54
JangMyungJoo <wkdaudwn11@naver.com>
2019-09-11 12:16:17 +0900

    Create email form and sending function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,109 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const Wrap = styled.div`
+  display: flex;
+  width: 1000px;
+  justify-content: center;
+  align-items: center;
+  background-color: #efefef;
+  margin: 200px auto;
+  flex-direction: column;
+
+  .title {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-basis: 100px;
+    font-size: 2rem;
+    font-weight: bold;
+  }
+
+  .contents {
+    flex: 1;
+    display: table;
+    padding: 1rem;
+  }
+
+  .contents span {
+    display: table-cell;
+    vertical-align: middle;
+  }
+
+  .contents input[type='email'] {
+    flex: 1;
+    width: 200px;
+    height: 25px;
+    margin-right: 10px;
+  }
+
+  .contents button {
+    flex: 1;
+    width: 100px;
+    height: 30px;
+    cursor: pointer;
+  }
+`;
 
 export default class App extends Component {
+  state = {
+    email: ''
+  };
+
+  inputEmail = null;
+
+  onChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .get('/mail', this.state.email)
+      .then(res => console.log(res.data))
+      .catch(err => this.setState({ errors: err.response.data }));
+  };
+
+  onKeyPress = e => {
+    if (e.key === 'Enter') {
+      this.onSubmit(e);
+    }
+  };
+
+  componentDidMount() {
+    this.inputEmail.focus();
+  }
+
   render() {
-    return <div>Welcome to AWS SES sample</div>;
+    return (
+      <Wrap>
+        <div className='title'>Welcome to AWS SES sample.</div>
+        <div className='contents'>
+          <form onSubmit={this.onSubmit}>
+            <span>
+              <input
+                type='email'
+                name='email'
+                placeholder='보내실 이메일을 입력해주세요.'
+                value={this.state.email}
+                onChange={this.onChange}
+                onKeyPress={this.onKeyPress}
+                ref={ref => {
+                  this.inputEmail = ref;
+                }}
+              />
+            </span>
+            <span>
+              <button type='submit'>이메일 보내기</button>
+            </span>
+          </form>
+        </div>
+      </Wrap>
+    );
   }
 }
